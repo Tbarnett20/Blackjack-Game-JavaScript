8433d4f5bdc11a99daf2b8c2a978b1ac0cd2669a
Terrance Danckert <tbarnett2020@gmail.com>
2018-10-28 23:15:21 -0400

    Added function to game**

--- main.js
+++ main.js
@@ -4,21 +4,30 @@
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
 let values = ['Ace','King','Queen','Jack','Ten','Nine','Eight','Seven','Six','Five','Four','Three','Two']
 
-let deck = [];
 
-for (let suitIDx = 0; suitIDx < suits.length; suitIDx++) {
-  for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
-    deck.push(`${values[valueIdx]} of ${suits[suitIDx]}`);
+createDeck = () => {
+  let deck = [];
+  for (let suitIDx = 0; suitIDx < suits.length; suitIDx++) {
+    for (let valueIdx = 0; valueIdx < values.length; valueIdx++) {
+      deck.push(`${values[valueIdx]} of ${suits[suitIDx]}`);
+    }
   }
+  return deck;
 }
+
+getNextCard = () => {
+  return deck.shift();
+}
+
+let deck = createDeck();
 
 for (let i = 0; i < deck.length; i++) {
   console.log(deck[i])
 }
 
 let playerCards = [
-  deck[0],
-  deck[2]
+ getNextCard(),
+ getNextCard()
 ];
 
 console.log("Welcome to Blackjack!")
